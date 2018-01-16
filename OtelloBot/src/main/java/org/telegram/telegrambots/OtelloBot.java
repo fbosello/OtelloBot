c30015f51e29816ef7210de970f3036304c5a379
Fabio Bosello <f.bosello.ecm@hitachi-systems-cbt.com>
2018-01-16 19:20:16 +0100

    Versione offensiva

--- OtelloBot/src/main/java/org/telegram/telegrambots/OtelloBot.java
+++ OtelloBot/src/main/java/org/telegram/telegrambots/OtelloBot.java
@@ -1,5 +1,7 @@
 package org.telegram.telegrambots;
 
+import java.util.Calendar;
+
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -18,9 +20,10 @@ public class OtelloBot extends TelegramLongPollingBot {
  
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
+        	Calendar cal = Calendar.getInstance();
+        	String messageStr = !((cal.getTimeInMillis() % 5) == 0) ? "You Said: " + update.getMessage().getText() : "Gives a shit";
             // Create a SendMessage object with mandatory fields
-            SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
-                    .setText("You Said: " + update.getMessage().getText());
+            SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId()).setText(messageStr);
             try {
                 sendMessage(message); // Call method to send the message
             } catch (TelegramApiException e) {
