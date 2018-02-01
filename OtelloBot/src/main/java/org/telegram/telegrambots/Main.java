ea13500ab9b52e48702c3d73496e89a5a07b3423
Fabio Bosello <f.bosello.ecm@hitachi-systems-cbt.com>
2018-02-01 23:25:31 +0100

    Integrazione log

--- OtelloBot/src/main/java/org/telegram/telegrambots/Main.java
+++ OtelloBot/src/main/java/org/telegram/telegrambots/Main.java
@@ -1,10 +1,13 @@
 package org.telegram.telegrambots;
 
+import org.apache.log4j.BasicConfigurator;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class Main {
 
+	
 	public static void main(String[] args) {
+		BasicConfigurator.configure();
         ApiContextInitializer.init();
  
         TelegramBotsApi botsApi = new TelegramBotsApi();
